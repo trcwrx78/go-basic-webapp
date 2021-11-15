5fcb83ae54292e14ad56b6210c4838acdc61ac63
Torrence Cole <trcole@gmail.com>
2021-11-15 14:34:30 -0500

    27. Reorganizing code

--- main.go
+++ main.go
@@ -3,29 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 const portNumber = ":8080"
-
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
-	}
-}
 
 // The main application function
 func main() {
