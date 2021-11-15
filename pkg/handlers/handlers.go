306e5799f07e8629547a3c7b08ba4f02f1f36ebd
Torrence Cole <trcole@gmail.com>
2021-11-15 16:46:16 -0500

    29 and 30 Template cache

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,15 +1,16 @@
-package main
+package handlers
 
 import (
+	"myapp/pkg/render"
 	"net/http"
 )
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.html")
+	 render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.html")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
