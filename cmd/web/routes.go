d87a0c3150dfb0548652824d8e8b1b80a2eeb72d
Torrence Cole <trcole@gmail.com>
2021-11-17 16:53:21 -0500

    finished intro middleware section

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,15 +6,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	// mux := pat.New()
-
-	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module myapp
 go 1.17
 
 require github.com/go-chi/chi/v5 v5.0.6
+
+require github.com/justinas/nosurf v1.1.1 // indirect
